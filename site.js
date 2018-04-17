42b5ca5d8521a88087faa9fbc693f22ce2fe09c7
Sean Kelliher <sbkelliher@icloud.com>
2018-04-17 12:27:40 -0400

    modify namespacing structure

--- site.js
+++ site.js
@@ -3,13 +3,13 @@ document.querySelector("form").addEventListener("change", function() {
     "use strict";
 
     //Invoke the "hide figures" function
-    figures.hideFigures();
+    senatorsFilter.hideFigures();
 
     //Invoke the "senators" and "parties" functions and get the intersection of the two results
-    let intersection1 = senators.getSenators().filter((x) => (parties.getParties()).includes(x));
+    let intersection1 = senatorsFilter.getSenators().filter((x) => (senatorsFilter.getParties()).includes(x));
 
     //Invoke the "genders" function and get the intersection of the above result and this result
-    let intersection2 = intersection1.filter((x) => genders.getGenders().includes(x));
+    let intersection2 = intersection1.filter((x) => senatorsFilter.getGenders().includes(x));
 
     //Display results - if no matches, display alert. If matches, display matches.
     let alert = document.getElementById("alert");
@@ -26,9 +26,9 @@ document.querySelector("form").addEventListener("change", function() {
     }
 });
 
-//The "hide figures" function. This function hides all figures on the page. (The other functions reveal them.)
-let figures = {
+let senatorsFilter = {
 
+    //The "hide figures" function. This function hides all figures on the page. (The other functions reveal them.)
     hideFigures: function() {
         "use strict";
 
@@ -38,60 +38,48 @@ let figures = {
         for (index=0; index < figure.length; index+=1) {
             figure[index].style.display = "none";
         }
-    }
-};
+    },
 
-//The "senators function". This function invokes the six "senators by state" functions and reveals which states' senators are selected (if no states are selected, it returns all senators)
-let senators = {
-
+    //The "senators function". This function invokes the six "senators by state" functions and reveals which states' senators are selected (if no states are selected, it returns all senators)
     getSenators: function() {
         "use strict";
 
         //Invoke functions - gather Senators by State and combine arrays with spread operator
-        let preSenators = [...senatorsCt.getSenatorsCt(), ...senatorsMe.getSenatorsMe(), ...senatorsMa.getSenatorsMa(), ...senatorsNh.getSenatorsNh(), ...senatorsRi.getSenatorsRi(), ...senatorsVt.getSenatorsVt()];
+        let preSenators = [...senatorsFilter.getSenatorsCt(), ...senatorsFilter.getSenatorsMe(), ...senatorsFilter.getSenatorsMa(), ...senatorsFilter.getSenatorsNh(), ...senatorsFilter.getSenatorsRi(), ...senatorsFilter.getSenatorsVt()];
 
         //Ternary statement for Senators
         let all = document.getElementsByClassName("all");
         let result = (preSenators.length === 0) ? Array.from(all, (a) => a.id) : preSenators;
         return result;
-    }
-};
+    },
 
-//The "parties function". This function invokes the three "senators by party" functions and reveals which parties' senators are selected (if no parties are, it returns all senators)
-let parties = {
-
+    //The "parties function". This function invokes the three "senators by party" functions and reveals which parties' senators are selected (if no parties are, it returns all senators)
     getParties: function() {
         "use strict";
 
         //Invoke functions - gather Senators by Party and combine arrays with spread operator
-        let preParties = [...senatorsDem.getSenatorsDem(), ...senatorsInd.getSenatorsInd(), ...senatorsRep.getSenatorsRep()];
+        let preParties = [...senatorsFilter.getSenatorsDem(), ...senatorsFilter.getSenatorsInd(), ...senatorsFilter.getSenatorsRep()];
 
         //Ternary statement for Parties
         let all = document.getElementsByClassName("all");
         let result = (preParties.length === 0) ? Array.from(all, (a) => a.id) : preParties;
         return result;
-    }
-};
+    },
 
-//The "genders function". This function invokes the two "senators by gender" functions and reveals which genders' senators are selected (if no genders are, it returns all senators)
-let genders = {
-
+    //The "genders function". This function invokes the two "senators by gender" functions and reveals which genders' senators are selected (if no genders are, it returns all senators)
     getGenders: function() {
         "use strict";
 
         //Invoke functions - gather Senators by Gender and combine arrays with spread operator
-        let preGenders = [...senatorsFemale.getSenatorsFemale(), ...senatorsMale.getSenatorsMale()];
+        let preGenders = [...senatorsFilter.getSenatorsFemale(), ...senatorsFilter.getSenatorsMale()];
 
         //Ternary statement for Genders
         let all = document.getElementsByClassName("all");
         let result = (preGenders.length === 0) ? Array.from(all, (a) => a.id) : preGenders;
         return result;
-    }
-};
+    },
 
-//The "senators function" for Connecticut. It gathers the senators from Connecticut, if Connecticut is selected. Otherwise, it returns an empty array.
-let senatorsCt = {
-
+    //The "senators function" for Connecticut. It gathers the senators from Connecticut, if Connecticut is selected. Otherwise, it returns an empty array.
     getSenatorsCt: function() {
         "use strict";
 
@@ -99,12 +87,9 @@ let senatorsCt = {
         let senators = document.getElementsByClassName("ct");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "senators function" for Maine. It gathers the senators from Maine, if Maine is selected. Otherwise, it returns an empty array.
-let senatorsMe = {
-
+    //The "senators function" for Maine. It gathers the senators from Maine, if Maine is selected. Otherwise, it returns an empty array.
     getSenatorsMe: function() {
         "use strict";
 
@@ -112,12 +97,9 @@ let senatorsMe = {
         let senators = document.getElementsByClassName("me");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "senators function" for Massachusetts. It gathers the senators from Massachusetts, if Massachusetts is selected. Otherwise, it returns an empty array.
-let senatorsMa = {
-
+    //The "senators function" for Massachusetts. It gathers the senators from Massachusetts, if Massachusetts is selected. Otherwise, it returns an empty array.
     getSenatorsMa: function() {
         "use strict";
 
@@ -125,12 +107,9 @@ let senatorsMa = {
         let senators = document.getElementsByClassName("ma");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "senators function" for New Hampshire. It gathers the senators from New Hampshire, if New Hampshire is selected. Otherwise, it returns an empty array.
-let senatorsNh = {
-
+    //The "senators function" for New Hampshire. It gathers the senators from New Hampshire, if New Hampshire is selected. Otherwise, it returns an empty array.
     getSenatorsNh: function() {
         "use strict";
 
@@ -138,12 +117,9 @@ let senatorsNh = {
         let senators = document.getElementsByClassName("nh");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "senators function" for Rhode Island. It gathers the senators from Rhode Island, if Rhode Island is selected. Otherwise, it returns an empty array.
-let senatorsRi = {
-
+    //The "senators function" for Rhode Island. It gathers the senators from Rhode Island, if Rhode Island is selected. Otherwise, it returns an empty array.
     getSenatorsRi: function() {
         "use strict";
 
@@ -151,12 +127,9 @@ let senatorsRi = {
         let senators = document.getElementsByClassName("ri");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "senators function" for Vermont. It gathers the senators from Vermont, if Vermont is selected. Otherwise, it returns an empty array.
-let senatorsVt = {
-
+    //The "senators function" for Vermont. It gathers the senators from Vermont, if Vermont is selected. Otherwise, it returns an empty array.
     getSenatorsVt: function() {
         "use strict";
 
@@ -164,12 +137,9 @@ let senatorsVt = {
         let senators = document.getElementsByClassName("vt");
         let result = (state.checked === true) ? Array.from(senators, (sens) => sens.id) : [];
         return result;
-    }
-};
+    },
 
-//The "parties function" for the Democratic Party. It gathers the senators from the Democratic Party, if the Democratic Party is selected. Otherwise, it returns an empty array.
-let senatorsDem = {
-
+    //The "parties function" for the Democratic Party. It gathers the senators from the Democratic Party, if the Democratic Party is selected. Otherwise, it returns an empty array.
     getSenatorsDem: function() {
         "use strict";
 
@@ -177,12 +147,9 @@ let senatorsDem = {
         let members = document.getElementsByClassName("democrat");
         let result = (party.checked === true) ? Array.from(members, (mems) => mems.id) : [];
         return result;
-    }
-};
+    },
 
-//The "parties function" for the Independent Party. It gathers the senators from the Independent Party, if the Independent Party is selected. Otherwise, it returns an empty array.
-let senatorsInd = {
-
+    //The "parties function" for the Independent Party. It gathers the senators from the Independent Party, if the Independent Party is selected. Otherwise, it returns an empty array.
     getSenatorsInd: function() {
         "use strict";
 
@@ -190,12 +157,9 @@ let senatorsInd = {
         let members = document.getElementsByClassName("independent");
         let result = (party.checked === true) ? Array.from(members, (mems) => mems.id) : [];
         return result;
-    }
-};
+    },
 
-//The "parties function" for the Republican Party. It gathers the senators from the Republican Party, if the Republican Party is selected. Otherwise, it returns an empty array.
-let senatorsRep = {
-
+    //The "parties function" for the Republican Party. It gathers the senators from the Republican Party, if the Republican Party is selected. Otherwise, it returns an empty array.
     getSenatorsRep: function() {
         "use strict";
 
@@ -203,12 +167,9 @@ let senatorsRep = {
         let members = document.getElementsByClassName("republican");
         let result = (party.checked === true) ? Array.from(members, (mems) => mems.id) : [];
         return result;
-    }
-};
+    },
 
-//The "genders function" for Female. It gathers the Female senators, if Female senators is selected. Otherwise, it returns an empty array.
-let senatorsFemale = {
-
+    //The "genders function" for Female. It gathers the Female senators, if Female senators is selected. Otherwise, it returns an empty array.
     getSenatorsFemale: function() {
         "use strict";
 
@@ -216,12 +177,9 @@ let senatorsFemale = {
         let group = document.getElementsByClassName("female");
         let result = (gender.checked === true) ? Array.from(group, (grp) => grp.id) : [];
         return result;
-    }
-};
+    },
 
-//The "genders function" for Male. It gathers the Male senators, if Male senators is selected. Otherwise, it returns an empty array.
-let senatorsMale = {
-
+    //The "genders function" for Male. It gathers the Male senators, if Male senators is selected. Otherwise, it returns an empty array.
     getSenatorsMale: function() {
         "use strict";
 
@@ -230,4 +188,5 @@ let senatorsMale = {
         let result = (gender.checked === true) ? Array.from(group, (grp) => grp.id) : [];
         return result;
     }
-};
+
+}; //close senatorsFilter
